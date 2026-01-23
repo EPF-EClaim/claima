8d5cea57a94a225c2ca979f514772a4465505740
Rushdi Abdul Rahman <rushdi.abdul.rahman@my.ey.com>
2026-01-23 10:21:30 +0000

    change view 'View1' into view 'Main'

--- app/claima-frontend/webapp/controller/Main.controller.js
+++ app/claima-frontend/webapp/controller/Main.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], (Controller) => {
     "use strict";
 
-    return Controller.extend("claimafrontend.controller.View1", {
+    return Controller.extend("claimafrontend.controller.Main", {
         onInit() {
         }
     });
