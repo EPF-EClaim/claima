337d1ceb5836ee2d907578612ab492292b40f3ba
nur-ain-mohamad <Nur.Ain.Mohamad@my.ey.com>
2026-02-26 02:32:41 +0000

    add on authentication for hybrid env

---
+++ app/Claim-A/webapp/controller/dashboard.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("claima.controller.dashboard", {
+        onInit() {
+            var that = this;
+        }
+    });
+});
