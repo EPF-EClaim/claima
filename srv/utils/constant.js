0334f26eca9a2e52eee983bf7fcb86fd5b1ef73c
nur-ain-mohamad <Nur.Ain.Mohamad@my.ey.com>
2026-03-19 15:39:41 +0000

    change of constant declaration

--- srv/utils/constant.js
+++ srv/utils/constant.js
@@ -1,10 +1,10 @@
-"use strict";
+module.exports = {
 
-const UserType = {
-    GA_ADMIN   : "GA Admin",
-    DTD_ADMIN  : "DTD Admin",
-    JKEW_ADMIN : "JKEW Admin",
-    APPROVER   : "Approver",
-    SUPER_ADMIN: "Super Admin"
-};
-module.exports = { UserType };
+    UserType: {
+        GA_ADMIN: "GA Admin",
+        DTD_ADMIN: "DTD Admin",
+        JKEW_ADMIN: "JKEW Admin",
+        APPROVER: "Approver",
+        SUPER_ADMIN: "Super Admin"
+    }
+}
