2cb4e2d89c42aff0f4b94e29c9382e96a953bd8b
Mei Shi Lim <jessica.lim2@my.ey.com>
2026-03-20 07:26:38 +0000

    Merge branch 'main' of https://github.com/EPF-EClaim/claima into JobScheduler

--- app/Claim-A/webapp/utils/EligibilityScenarios/ComparisonOperators.js
+++ app/Claim-A/webapp/utils/EligibilityScenarios/ComparisonOperators.js
@@ -9,33 +9,9 @@ sap.ui.define([
                 return false;
             }
         },
-        
-        NotEqualsTo(sVal1, sVal2){
-            if( sVal1 != sVal2 ){
-                return false;
-            }else{
-                return true;
-            }
-        },
-
-        Greater(sVal1, sVal2){
-            if( sVal1 > sVal2 ){
-                return true;
-            }else{
-                return sVal2;
-            }
-        },
 
         GreaterEquals(sVal1, sVal2){
             if( sVal1 >= sVal2 ){
-                return true;
-            }else{
-                return sVal2;
-            }
-        },
-
-        Lesser(sVal1, sVal2){
-            if( sVal1 < sVal2 ){
                 return true;
             }else{
                 return sVal2;
--- app/Claim-A/webapp/utils/EligibilityScenarios/DalamNegara.js
+++ app/Claim-A/webapp/utils/EligibilityScenarios/DalamNegara.js
@@ -1,42 +1,39 @@
 sap.ui.define([
-], function () {
+    "./ComparisonOperators"
+], function (ComparisonOperators) {
     "use strict";
 
     return {
         onEligibleCheck(oConstant, oPayload, aRules) {
 
-            var oRule, aFilteredRules, aResults;
+            var oRule, aFilteredRules;
 
             var aPayload = this._parsePayload(oPayload, oConstant);
 
             if (aPayload.CLAIM_TYPE_ITEM_ID === oConstant.ClaimTypeItem.FLIGHT_L) {
-
                 aFilteredRules = aRules.filter(function (rule) {
                     return rule.FLIGHT_CLASS_ID === aPayload[oConstant.FIELDNAME.FLIGHT_CLASS_ID];
                 })
 
                 oRule = aFilteredRules[0];
-
-            } 
+            }
             else if (aPayload.CLAIM_TYPE_ITEM_ID === oConstant.ClaimTypeItem.TAMBANG) {
-
                 aFilteredRules = aRules.filter(function (rule) {
                     return rule.TRANSPORT_CLASS === aPayload[oConstant.FIELDNAME.FARE_TYPE_ID];
                 })
 
                 oRule = aFilteredRules[0];
-
             }
             else {
                 oRule = aRules[0];
             }
 
-            aResults = this._validateClaimItem(aPayload, oConstant, oRule);
-            return aResults;
+            this._validateClaimItem(aPayload, oConstant, oRule, oPayload);
+            return oPayload;
 
         },
 
-        _parsePayload(oPayload, oConstant){
+        _parsePayload(oPayload, oConstant) {
             var CLAIM_TYPE_ITEM_ID, TRAVEL_DAYS_ID, ELIGIBLE_AMOUNT, FLIGHT_CLASS_ID, FARE_TYPE_ID;
 
             CLAIM_TYPE_ITEM_ID = oPayload.ClaimTypeItem;
@@ -53,94 +50,65 @@ sap.ui.define([
                 }
             }
 
-            return { CLAIM_TYPE_ITEM_ID, TRAVEL_DAYS_ID, ELIGIBLE_AMOUNT, FLIGHT_CLASS_ID, FARE_TYPE_ID  };
+            return { CLAIM_TYPE_ITEM_ID, TRAVEL_DAYS_ID, ELIGIBLE_AMOUNT, FLIGHT_CLASS_ID, FARE_TYPE_ID };
         },
-        _validateClaimItem(aPayload, oConstant, oEligibilityRule) {
+        _validateClaimItem(aPayload, oConstant, oRule, oPayload) {
+            var iIndex;
 
-            var oValidators = {
-                [oConstant.ClaimTypeItem.DOBI]: {
-                    payloadField: oConstant.FIELDNAME.TRAVEL_DAYS_ID,
-                    eligibilityRuleField: oConstant.FIELDNAME.TRAVEL_DAYS_ID,
-                    type: oConstant.VALIDATION_TYPE.MIN,
-                },
-                [oConstant.ClaimTypeItem.FLIGHT_L]: {
-                    payloadField: oConstant.FIELDNAME.FLIGHT_CLASS_ID,
-                    eligibilityRuleField: oConstant.FIELDNAME.FLIGHT_CLASS_ID,
-                    type: oConstant.VALIDATION_TYPE.EQUAL,
-                },
-                [oConstant.ClaimTypeItem.HOTEL_L]: {
-                    payloadField: oConstant.FIELDNAME.ELIGIBLE_AMOUNT,
-                    eligibilityRuleField: oConstant.FIELDNAME.ELIGIBLE_AMOUNT,
-                    type: oConstant.VALIDATION_TYPE.MAX,
-                },
-                [oConstant.ClaimTypeItem.LODGING_L]: {
-                    payloadField: oConstant.FIELDNAME.ELIGIBLE_AMOUNT,
-                    eligibilityRuleField: oConstant.FIELDNAME.ELIGIBLE_AMOUNT,
-                    type: oConstant.VALIDATION_TYPE.MAX,
-                },
-                [oConstant.ClaimTypeItem.TAMBANG]: {
-                    payloadField: oConstant.FIELDNAME.FARE_TYPE_ID,
-                    eligibilityRuleField: oConstant.FIELDNAME.TRANSPORT_CLASS,
-                    type: oConstant.VALIDATION_TYPE.EQUAL,
-                },
-            };
-            var oValidator = oValidators[aPayload.CLAIM_TYPE_ITEM_ID];
+            switch (aPayload.CLAIM_TYPE_ITEM_ID) {
+                case oConstant.ClaimTypeItem.DOBI:
+                    iIndex = oPayload.CheckFields.findIndex((field) =>
+                        field.fieldName === oConstant.FIELDNAME.TRAVEL_DAYS_ID);
+                    if (!oRule) {
+                        oPayload.CheckFields[iIndex].result = false;
+                    } else {
+                        oPayload.CheckFields[iIndex].result = ComparisonOperators.GreaterEquals(
+                            oPayload.CheckFields[iIndex].value,
+                            oRule.TRAVEL_DAYS_ID
+                        );
+                    }
+                    break;
 
-            return this._validateField(aPayload, oConstant, oEligibilityRule, oValidator);
+                case oConstant.ClaimTypeItem.FLIGHT_L:
+                    iIndex = oPayload.CheckFields.findIndex((field) =>
+                        field.fieldName === oConstant.FIELDNAME.FLIGHT_CLASS_ID);
+                    if (!oRule) {
+                        oPayload.CheckFields[iIndex].result = false;
+                    } else {
+                        oPayload.CheckFields[iIndex].result = ComparisonOperators.EqualsTo(
+                            oPayload.CheckFields[iIndex].value,
+                            oRule.FLIGHT_CLASS_ID
+                        );
+                    }
+                    break;
 
-        },
+                case oConstant.ClaimTypeItem.HOTEL_L:
+                case oConstant.ClaimTypeItem.LODGING_L:
+                    iIndex = oPayload.CheckFields.findIndex((field) =>
+                        field.fieldName === oConstant.FIELDNAME.ELIGIBLE_AMOUNT);
+                    if (!oRule) {
+                        oPayload.CheckFields[iIndex].result = false;
+                    } else {
+                        oPayload.CheckFields[iIndex].result = ComparisonOperators.LesserEquals(
+                            parseFloat(oPayload.CheckFields[iIndex].value),
+                            parseFloat(oRule.ELIGIBLE_AMOUNT) * parseFloat(aPayload.TRAVEL_DAYS_ID)
+                        );
+                    }
+                    break;
 
-        _validateField(aPayload, oConstant, oEligibilityRule, oValidator) {
-
-            var sPayloadField = oValidator.payloadField;
-            var sEligibilityRuleField = oValidator.eligibilityRuleField;
-            var sType = oValidator.type;
-            var eligibilityRuleValue, userValue;
-
-            let result; 
-
-            if (!oEligibilityRule) {
-                result = false;
-                return [{ field: sPayloadField, value: userValue, result: result }];
-            } else {
-                eligibilityRuleValue = oEligibilityRule[sEligibilityRuleField];
+                case oConstant.ClaimTypeItem.TAMBANG:
+                    iIndex = oPayload.CheckFields.findIndex((field) =>
+                        field.fieldName === oConstant.FIELDNAME.FARE_TYPE_ID);
+                    if (!oRule) {
+                        oPayload.CheckFields[iIndex].result = false;
+                    } else {
+                        oPayload.CheckFields[iIndex].result = ComparisonOperators.EqualsTo(
+                            oPayload.CheckFields[iIndex].value,
+                            oRule.TRANSPORT_CLASS
+                        );
+                    }
+                    break;  
             }
-
-            switch (sType) {
-                // ✅ Must match exactly — Flight class, Tambang class
-                case oConstant.VALIDATION_TYPE.EQUAL:
-                    userValue = aPayload[oConstant.FIELDNAME.FLIGHT_CLASS_ID] || aPayload[oConstant.FIELDNAME.FARE_TYPE_ID];
-
-                    if (userValue !== eligibilityRuleValue) {
-                        result = false;
-                    } else { result = true; }
-
-                    break;
-
-                // ✅ Must not exceed — Hotel, Lodging
-                case oConstant.VALIDATION_TYPE.MAX:
-                    userValue = aPayload[oConstant.FIELDNAME.ELIGIBLE_AMOUNT];
-
-                    var eligibilityRuleValue = eligibilityRuleValue * aPayload[oConstant.FIELDNAME.TRAVEL_DAYS_ID];
-                    if (parseFloat(userValue) > parseFloat(eligibilityRuleValue)) {
-                        result = eligibilityRuleValue;
-                    } else { result = true; }
-
-                    break;
-
-                // ✅ must exceed min — Dobi
-                case oConstant.VALIDATION_TYPE.MIN:
-                    userValue = aPayload[oConstant.FIELDNAME.TRAVEL_DAYS_ID];
-
-                    if (userValue < eligibilityRuleValue) {
-                        result = eligibilityRuleValue;
-                    } else { result = true; }
-
-                    break;
-            }
-            return [{ field: sPayloadField, value: userValue, result }];
-
         }
-
     };
 });
