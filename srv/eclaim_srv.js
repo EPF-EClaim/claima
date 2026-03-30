668c3277500bdcd63b7247fac04ba9a86ead421f
Nur Ain Mohamad <nur.ain.mohamad@my.ey.com>
2026-03-30 11:13:04 +0000

    authorization JKEW

--- srv/eclaim_srv.js
+++ srv/eclaim_srv.js
@@ -125,10 +125,8 @@ module.exports = (srv) => {
         const user_type = result?.USER_TYPE;
 
         let operationHidden = true;
-        // if (user_type === Constant.UserType.JKEW_ADMIN) {
         if(req.user.is(Constant.Admin.Admin_System)) {
             operationHidden = true;
-        // } else if (user_type === Constant.UserType.DTD_ADMIN || user_type === Constant.UserType.SUPER_ADMIN) {
         } else if (req.user.is(Constant.Admin.DTD_Admin)) {
             operationHidden = false;
         }
@@ -146,7 +144,11 @@ module.exports = (srv) => {
         const result = await SELECT.one.from(ZEMP_MASTER).where({ EMAIL: email });
         const user_type = result?.USER_TYPE;
 
-        let operationHidden = (user_type === Constant.UserType.GA_ADMIN);
+        // let operationHidden = (user_type === Constant.UserType.GA_ADMIN);
+        let operationHidden = false;
+        if(req.user.is(Constant.Admin.Admin_CC)) {
+            operationHidden = true;
+        }
         return {
             operationHidden: operationHidden,
             operationEnabled: !operationHidden,
