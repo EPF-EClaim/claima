918c41922abd085005891e8ca889d1eec8f63fe2
Rushdi Abdul Rahman <rushdi.abdul.rahman@my.ey.com>
2026-04-02 12:26:51 +0000

    moved method to new utility file

--- app/Claim-A/webapp/controller/ClaimSubmission.controller.js
+++ app/Claim-A/webapp/controller/ClaimSubmission.controller.js
@@ -16,6 +16,7 @@ sap.ui.define([
 	"sap/m/Label",
 	"sap/m/ListMode",
 	"claima/utils/Utility",
+	"claima/utils/ClaimUtility",
 	"claima/utils/Attachment",
 	"claima/utils/budgetCheck",
 	"claima/utils/ApprovalLog",
@@ -45,6 +46,7 @@ sap.ui.define([
 	Label,
 	ListMode,
 	Utility,
+	ClaimUtility,
 	Attachment,
 	budgetCheck,
 	ApprovalLog,
@@ -2079,7 +2081,7 @@ sap.ui.define([
 			// set percentage (%) compensation based on claim item
 			var oPropertyModel = this.getView().getModel("claimitem_property");
 			if (oPropertyModel.getProperty("/percentage_compensation/is_visible")) {
-				this._setPresetValues_ClaimItem("percentage_compensation", this._oConstant.EligibilityRule.SUBSIDISED_RATE, 0.0)
+				ClaimUtility.setClaimItemDefaultValues(this, "percentage_compensation", this._oConstant.EligibilityRule.SUBSIDISED_RATE, 0.0)
 			}
 		},
 
@@ -2239,70 +2241,6 @@ sap.ui.define([
 						text: "{employee>" + oField + "_ID} - {employee>" + oField + "_DESC}"
 					})
 				});
-			}
-		},
-
-		/**
-        * Set preset values for claim item fields
-        * Request is made to get values from table ZELIGIBILITY_RULE, based on user role and claim type/claim item given 
-        * if record found, value is retrieved from the table and populated in the claim item model
-        * @private
-		* @param {string} sClaimItemField - claim item field to be populated
-		* @param {string} sEligibilityRule - field to retrieve value from db table
-		* @param {string} sDefaultValue - default value to set if none found
-        */
-		_setPresetValues_ClaimItem: async function (sClaimItemField, sEligibilityRule, sDefaultValue) {
-			var oClaimSubmissionModel = this.getView().getModel("claimsubmission_input");
-			var oInputModel = this.getView().getModel("claimitem_input");
-			const oModel = this.getOwnerComponent().getModel();
-			//// filter by employee role ID or * (all)
-			const oFilterRoleId = new Filter({
-				filters: [
-					new Filter("ROLE_ID", FilterOperator.EQ, oClaimSubmissionModel.getProperty("/emp_master/role")),
-					new Filter("ROLE_ID", FilterOperator.EQ, '*')
-				],
-				and: false
-			});
-			//// filter by employee role ID or * (all)
-			const oFilterPersonalGrade = new Filter({
-				filters: [
-					new Filter("PERSONAL_GRADE", FilterOperator.EQ, oClaimSubmissionModel.getProperty("/emp_master/grade")),
-					new Filter("PERSONAL_GRADE", FilterOperator.EQ, '*')
-				],
-				and: false
-			});
-			const oListBinding = oModel.bindList("/ZELIGIBILITY_RULE", null, [
-				new Sorter("ROLE_ID", true),
-				new Sorter("PERSONAL_GRADE", true),
-				new Sorter("POSITION_NO_DESC", true),
-				new Sorter("ROW_COUNT", true),
-			], [
-				new Filter("CLAIM_TYPE_ID", FilterOperator.EQ, oInputModel.getProperty("/claim_item/claim_type_id")),
-				new Filter("CLAIM_TYPE_ITEM_ID", FilterOperator.EQ, oInputModel.getProperty("/claim_item/claim_type_item_id")),
-				oFilterRoleId,
-				oFilterPersonalGrade,
-				// ensure status is active
-				new Filter("STATUS", FilterOperator.EQ, this._oConstant.ClaimTypeItemStatus.ACTIVE),
-				new Filter("START_DATE", FilterOperator.LE, DateUtility.getHanaDate(DateUtility.today())),
-				new Filter("END_DATE", FilterOperator.GE, DateUtility.getHanaDate(DateUtility.today())),
-			]);
-
-			try {
-				BusyIndicator.show(0);
-				const aContexts = await oListBinding.requestContexts(0, Infinity);
-
-				if (aContexts.length > 0) {
-					const oData = aContexts[0].getObject();
-					oInputModel.setProperty("/claim_item/" + sClaimItemField, oData[sEligibilityRule]);
-				} else {
-					oInputModel.setProperty("/claim_item/" + sClaimItemField, sDefaultValue);
-					MessageToast.show(Utility.getText("msg_claimdetails_input_" + sClaimItemField + "_none"));
-				}
-			} catch (oError) {
-				oInputModel.setProperty("/claim_item/percentage_compensation", 0.0);
-				MessageBox.error(Utility.getText("msg_claimdetails_input_" + sClaimItemField + "_err", [oError]));
-			} finally {
-				BusyIndicator.hide();
 			}
 		},
 
---
+++ app/Claim-A/webapp/utils/ClaimUtility.js
@@ -0,0 +1,86 @@
+sap.ui.define([
+	"sap/ui/model/Sorter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/core/BusyIndicator",
+	"sap/m/MessageBox",
+	"sap/m/MessageToast",
+	"claima/utils/DateUtility"
+], function (
+    Sorter,
+	Filter,
+	FilterOperator,
+	BusyIndicator,
+    MessageBox,
+    MessageToast,
+    DateUtility
+) {
+	"use strict";
+
+    return {
+
+		/**
+        * Set default values for claim item fields
+        * Request is made to get values from table ZELIGIBILITY_RULE, based on user role and claim type/claim item given 
+        * if record found, value is retrieved from the table and populated in the claim item model
+        * @public
+		* @param {string} sClaimItemField - claim item field to be populated
+		* @param {string} sEligibilityRule - field to retrieve value from db table
+		* @param {string} sDefaultValue - default value to set if none found
+        */
+		setClaimItemDefaultValues: async function (oController, sClaimItemField, sEligibilityRule, sDefaultValue) {
+			var oClaimSubmissionModel = oController.getView().getModel("claimsubmission_input");
+			var oInputModel = oController.getView().getModel("claimitem_input");
+			const oModel = oController.getOwnerComponent().getModel();
+			//// filter by employee role ID or * (all)
+			const oFilterRoleId = new Filter({
+				filters: [
+					new Filter("ROLE_ID", FilterOperator.EQ, oClaimSubmissionModel.getProperty("/emp_master/role")),
+					new Filter("ROLE_ID", FilterOperator.EQ, '*')
+				],
+				and: false
+			});
+			//// filter by employee role ID or * (all)
+			const oFilterPersonalGrade = new Filter({
+				filters: [
+					new Filter("PERSONAL_GRADE", FilterOperator.EQ, oClaimSubmissionModel.getProperty("/emp_master/grade")),
+					new Filter("PERSONAL_GRADE", FilterOperator.EQ, '*')
+				],
+				and: false
+			});
+			const oListBinding = oModel.bindList("/ZELIGIBILITY_RULE", null, [
+				new Sorter("ROLE_ID", true),
+				new Sorter("PERSONAL_GRADE", true),
+				new Sorter("POSITION_NO_DESC", true),
+				new Sorter("ROW_COUNT", true),
+			], [
+				new Filter("CLAIM_TYPE_ID", FilterOperator.EQ, oInputModel.getProperty("/claim_item/claim_type_id")),
+				new Filter("CLAIM_TYPE_ITEM_ID", FilterOperator.EQ, oInputModel.getProperty("/claim_item/claim_type_item_id")),
+				oFilterRoleId,
+				oFilterPersonalGrade,
+				// ensure status is active
+				new Filter("STATUS", FilterOperator.EQ, oController._oConstant.ClaimTypeItemStatus.ACTIVE),
+				new Filter("START_DATE", FilterOperator.LE, DateUtility.getHanaDate(DateUtility.today())),
+				new Filter("END_DATE", FilterOperator.GE, DateUtility.getHanaDate(DateUtility.today())),
+			]);
+
+			try {
+				BusyIndicator.show(0);
+				const aContexts = await oListBinding.requestContexts(0, Infinity);
+
+				if (aContexts.length > 0) {
+					const oData = aContexts[0].getObject();
+					oInputModel.setProperty("/claim_item/" + sClaimItemField, oData[sEligibilityRule]);
+				} else {
+					oInputModel.setProperty("/claim_item/" + sClaimItemField, sDefaultValue);
+					MessageToast.show(Utility.getText("msg_claimdetails_input_" + sClaimItemField + "_none"));
+				}
+			} catch (oError) {
+				oInputModel.setProperty("/claim_item/percentage_compensation", 0.0);
+				MessageBox.error(Utility.getText("msg_claimdetails_input_" + sClaimItemField + "_err", [oError]));
+			} finally {
+				BusyIndicator.hide();
+			}
+		}
+    }
+});
