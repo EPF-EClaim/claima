1add3b6669c2e4bd2db702ec36d1e9ff6a20a3ff
Farisha Razak <farisha.razak@my.ey.com>
2026-04-02 07:16:56 +0000

    FUT issue #108 and UAT issue #46

--- app/Claim-A/webapp/controller/ClaimSubmission.controller.js
+++ app/Claim-A/webapp/controller/ClaimSubmission.controller.js
@@ -9,11 +9,13 @@ sap.ui.define([
 	"sap/ui/model/FilterOperator",
 	"sap/ui/model/Sorter",
 	"sap/ui/core/format/DateFormat",
+	"sap/ui/core/ValueState",
 	"sap/m/MessageBox",
 	"sap/m/MessageToast",
 	"sap/m/Dialog",
 	"sap/m/Button",
 	"sap/m/Label",
+	"sap/m/FormattedText",
 	"sap/m/ListMode",
 	"claima/utils/Utility",
 	"claima/utils/Attachment",
@@ -38,11 +40,13 @@ sap.ui.define([
 	FilterOperator,
 	Sorter,
 	DateFormat,
+	ValueState,
 	MessageBox,
 	MessageToast,
 	Dialog,
 	Button,
 	Label,
+	FormattedText,
 	ListMode,
 	Utility,
 	Attachment,
@@ -1513,14 +1517,25 @@ sap.ui.define([
 						return;
 					}
 
+					var sDeclarationHTML =
+						"<ul>" +
+						"<li>" + Utility.getText("msg_claim_decl_item1_en") + "</li>" +
+						"<li>" + Utility.getText("msg_claim_decl_item2_en") + "</li>" +
+						"<li>" + Utility.getText("msg_claim_decl_item3_en") + "</li>" +
+						"<li>" + Utility.getText("msg_claim_decl_item4_en") + "</li>" +
+						"<li>" + Utility.getText("msg_claim_decl_item5_en") + "</li>" +
+						"<li>" + Utility.getText("msg_claim_decl_item6_en") + "</li>" +
+						"</ul>";
+
 					// confirm dialog
-					this._newDialog(
+					this._oDeclarationDialog(
 						Utility.getText("dialog_claimsubmission_submitreport"),
-						Utility.getText("label_claimsubmission_submitreport"),
+						sDeclarationHTML,
 						function () {
 							this._updateClaimSubmission(oAction);
 						}.bind(this)
 					);
+
 					break;
 				//// Back
 				case 'Back':
@@ -2041,7 +2056,7 @@ sap.ui.define([
 			var oInputModel = this.getView().getModel("claimitem_input");
 			if (claimItem) {
 				// Reset Location Type
-	       		oInputModel.setProperty("/claim_item/location_type", "");
+				oInputModel.setProperty("/claim_item/location_type", "");
 
 				// get material code from claim item
 				var materialCode = claimItem.getBindingContext("employee").getObject("MATERIAL_CODE");
@@ -2050,7 +2065,7 @@ sap.ui.define([
 
 			// set app visibility controls
 			await this.getFieldVisibility_ClaimTypeItem();
-			
+
 			// When Location Type is visible but no selection yet, hide From State & To State by default
 			// If show, then State will be Select but Location will be input, which inconsistent from UI
 			if (this.byId("select_claimdetails_input_location_type").getVisible()) {
@@ -2153,7 +2168,7 @@ sap.ui.define([
 			this.byId("select_claimdetails_input_claimitem").bindAggregation("items", {
 				path: "employee>/ZCLAIM_TYPE_ITEM",
 				filters: [
-					new Filter('CLAIM_TYPE_ID', FilterOperator.EQ,oModel.getProperty("/claim_header/claim_type_id")),
+					new Filter('CLAIM_TYPE_ID', FilterOperator.EQ, oModel.getProperty("/claim_header/claim_type_id")),
 					oFilterSubsmissionType,
 					// ensure status is active
 					new Filter("STATUS", FilterOperator.EQ, this._oConstant.ClaimTypeItemStatus.ACTIVE),
@@ -2451,7 +2466,7 @@ sap.ui.define([
 			var dTripEndDate = new Date(oClaimSubmissionModel.getProperty("/claim_header/trip_end_date")).toLocaleDateString('en-CA');
 			var dReceiptDate = new Date(oInputModel.getProperty("/claim_item/receipt_date")).toLocaleDateString('en-CA');
 
-			if(dReceiptDate > dTripEndDate){
+			if (dReceiptDate > dTripEndDate) {
 				MessageToast.show(Utility.getText("msg_claimsubmission_invalid_receipt_date"));
 				return;
 			}
@@ -2467,7 +2482,7 @@ sap.ui.define([
 			var dTripEndDate = new Date(oClaimSubmissionModel.getProperty("/claim_header/trip_end_date")).toLocaleDateString('en-CA');
 			var dReceiptDate = new Date(oInputModel.getProperty("/claim_item/receipt_date")).toLocaleDateString('en-CA');
 
-			if(dReceiptDate > dTripEndDate){
+			if (dReceiptDate > dTripEndDate) {
 				MessageToast.show(Utility.getText("msg_claimsubmission_invalid_receipt_date"));
 				return;
 			}
@@ -2715,13 +2730,13 @@ sap.ui.define([
 			}
 		},
 
-        /**
-         * Determine which method to use when calculating number of days for claim item
+		/**
+		 * Determine which method to use when calculating number of days for claim item
 		 * if claim item is Dobi, pass start/end date value from claim header
 		 * else if header is empty, pass start/end date value from claim item
-         * @private
-         * @return {integer} retrieve number of days value based on start/end date from claim
-         */
+		 * @private
+		 * @return {integer} retrieve number of days value based on start/end date from claim
+		 */
 		_calculateNumberOfDays: function () {
 			var oHeader = {};
 			var oItem = {};
@@ -3840,43 +3855,72 @@ sap.ui.define([
 			this.oDialog.open();
 		},
 
+		//Declaration dialog
+		_oDeclarationDialog: function (oTitle, oHTMLContent, onPress, onPressE) {
+			this.oDialog = new Dialog({
+				title: oTitle,
+				type: "Message",
+				state: ValueState.Warning,
+				contentWidth: "500px",
+				content: [new FormattedText({ htmlText: oHTMLContent, wrapping: true })],
+				beginButton: new Button({
+					type: "Emphasized",
+					text: Utility.getText("button_claimsubmission_declaration_confirm"),
+					press: async function () {
+						this.oDialog.close();
+						await onPress();
+					}.bind(this)
+				}),
+				endButton: new Button({
+					text: Utility.getText("button_claimsummary_cancel"),
+					press: async function () {
+						this.oDialog.close();
+						if (onPressE) {
+							await onPressE();
+						}
+					}.bind(this)
+				})
+			});
+			this.oDialog.open();
+		},
+
 		getFromLocationOfficeByState: function () {
-            var _oSelect = this.byId("select_claimdetails_input_from_location");
-            var _oBinding = _oSelect.getBinding("items");
-            if (!_oBinding) {
-                return;
-            }
+			var _oSelect = this.byId("select_claimdetails_input_from_location");
+			var _oBinding = _oSelect.getBinding("items");
+			if (!_oBinding) {
+				return;
+			}
 
 			var _oInputModel = this.getView().getModel("claimitem_input");
 			if (!_oInputModel) {
 				return;
 			}
 
-            var _aFilters = [
-                new Filter("STATUS", FilterOperator.EQ, this._oConstant.Status.ACTIVE),
+			var _aFilters = [
+				new Filter("STATUS", FilterOperator.EQ, this._oConstant.Status.ACTIVE),
 				new Filter("STATE_ID", FilterOperator.EQ, _oInputModel.getProperty("/claim_item/from_state_id"))
-            ];
-            _oBinding.filter(_aFilters);
-        },
+			];
+			_oBinding.filter(_aFilters);
+		},
 
 		getToLocationOfficeByState: function () {
-            var _oSelect = this.byId("select_claimdetails_input_to_location");
-            var _oBinding = _oSelect.getBinding("items");
-            if (!_oBinding) {
-                return;
-            }
+			var _oSelect = this.byId("select_claimdetails_input_to_location");
+			var _oBinding = _oSelect.getBinding("items");
+			if (!_oBinding) {
+				return;
+			}
 
 			var _oInputModel = this.getView().getModel("claimitem_input");
 			if (!_oInputModel) {
 				return;
 			}
 
-            var _aFilters = [
-                new Filter("STATUS", FilterOperator.EQ, this._oConstant.Status.ACTIVE),
+			var _aFilters = [
+				new Filter("STATUS", FilterOperator.EQ, this._oConstant.Status.ACTIVE),
 				new Filter("STATE_ID", FilterOperator.EQ, _oInputModel.getProperty("/claim_item/to_state_id"))
-            ];
-            _oBinding.filter(_aFilters);
-        },
+			];
+			_oBinding.filter(_aFilters);
+		},
 
 		// App Control Visibility
 		getFieldVisibility_ClaimTypeItem: async function () {
