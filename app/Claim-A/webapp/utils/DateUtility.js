48949f6715dcab578b2d49ce88577250cc9b9bd4
Farisha Razak <farisha.razak@my.ey.com>
2026-04-12 03:05:04 +0000

    Moved isNightBasedCalculation to Utility.js

--- app/Claim-A/webapp/utils/DateUtility.js
+++ app/Claim-A/webapp/utils/DateUtility.js
@@ -176,7 +176,7 @@ sap.ui.define([
             if (iEndMidnight >= iStartMidnight) {
                 const iMsPerDay = 1000 * 60 * 60 * 24;
                 const iBaseDiff = Math.floor((iEndMidnight - iStartMidnight) / iMsPerDay);
-                const bIsNightBased = this.isNightBasedCalculation(oHeader,oItem);
+                const bIsNightBased = Utility.isNightBasedCalculation(oHeader,oItem);
                 iDiffDays = bIsNightBased ? iBaseDiff : iBaseDiff + 1;
             }
 
@@ -759,41 +759,5 @@ sap.ui.define([
                 .toString()
                 .padStart(2, '0')}:${iSeconds.toString().padStart(2, '0')}`;
         },
-
-        
-        /**
-         * Determine whether the number of days calculation should be treated as
-         * "number of nights" instead of inclusive calendar days.
-         *
-         * applies ONLY to specific claim types
-         *
-         * When this method returns true, the date difference will be calculated
-         * without adding 1 day (end date minus start date = number of nights).
-         *
-         * @public
-         * @param {object} oHeader - claim header data containing claim type information
-         * @param {object} oItem - claim item data containing claim type item ID
-         * @return {boolean} bIsNightBased - true if calculation should be based on number of nights
-         */
-
-        isNightBasedCalculation: function (oHeader, oItem) {
-            const aNightClaimTypes = [
-                Constants.ClaimType.DLM_NEGARA,
-                Constants.ClaimType.LUAR_NEGARA,
-                Constants.ClaimType.KURSUS_DLM_NEGARA,
-                Constants.ClaimType.KURSUS_LUAR_NEGARA
-            ];
-
-            const aLodgingItems = [
-                Constants.ClaimTypeItem.HOTEL_L,
-                Constants.ClaimTypeItem.HOTEL_O,
-                Constants.ClaimTypeItem.LODGING_L,
-                Constants.ClaimTypeItem.LODG_O
-            ];
-
-            return aNightClaimTypes.includes(oHeader.claim_type_id) &&
-                aLodgingItems.includes(oItem.claim_type_item_id);
-        },
-
     };
 });
