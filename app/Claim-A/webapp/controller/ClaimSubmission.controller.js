e93506220b099e3e5327a7f32242df5a1b05eab6
Vincent Yong <dick.soon.yong@my.ey.com>
2026-04-12 04:21:40 +0000

    swap back to select instead of valuehelp

--- app/Claim-A/webapp/controller/ClaimSubmission.controller.js
+++ app/Claim-A/webapp/controller/ClaimSubmission.controller.js
@@ -1768,7 +1768,7 @@ sap.ui.define([
 			const aItemsColumnsAdditional = [
 				{ label: Utility.getText("label_claimdetails_input_depedent_or_anggota"), property: "dependent_type", field: "select_claimdetails_input_depedent_or_anggota", width: 30 },
 				{ label: Utility.getText("label_claimdetails_input_anggota"), property: "anggota_name", field: "field_claimdetails_input_anggota_name", width: 30 },
-				{ label: Utility.getText("label_claimdetails_input_dependent"), property: "dependent_name", field: "field_claimdetails_input_dependent_name", width: 30 },
+				{ label: Utility.getText("label_claimdetails_input_dependent"), property: "dependent_name", field: "select_claimdetails_input_dependent_name", width: 30 },
 				{ label: Utility.getText("label_claimdetails_input_profbodytype"), property: "type_of_professional_body", field: "select_claimdetails_input_type_of_professional_body", type: "descr", width: 40 },
 				{ label: Utility.getText("label_claimdetails_input_policyno"), property: "policy_number", field: "input_claimdetails_input_policy_number", width: 18 },
 				{ label: Utility.getText("label_claimdetails_input_funeraltransport"), property: "funeral_transportation", field: "select_claimdetails_input_funeral_transportation", type: "descr", width: 18 },
@@ -2466,6 +2466,7 @@ sap.ui.define([
 		},
 
 		_setClaimDetailSelectionMaster: function () {
+			var oInputModel = this._getNewClaimItemModel("claimitem_input");
 			//// Type of Professional Body
 			this._setClaimDetailSelectionField("select_claimdetails_input_type_of_professional_body", "ZPROFESIONAL_BODY");
 			//// Funeral Transportation
@@ -2500,6 +2501,27 @@ sap.ui.define([
 			this._setClaimDetailSelectionField("select_claimdetails_input_claim_category", "ZCLAIM_CATEGORY");
 			//// Category/Purpose (Mobile)
 			this._setClaimDetailSelectionField("select_claimdetails_input_mobile_category_purpose_id", "ZMOBILE_CATEGORY_PURPOSE");
+
+			var oSelect = this.byId("select_claimdetails_input_dependent_name");
+			var oBinding = oSelect.getBinding("items");
+			
+			var aFilters = [
+				new Filter('EMP_ID', FilterOperator.EQ, this._oSessionModel.getProperty("/userId"))
+			]
+
+			if(oInputModel.getProperty("/claim_item/claim_type_item_id") === this._oConstant.ClaimTypeItem.POST_EDUCATION_ASSISTANCE){
+				aFilters =[
+					new Filter('EMP_ID', FilterOperator.EQ, this._oSessionModel.getProperty("/userId")),
+					new Filter("RELATIONSHIP" , FilterOperator.EQ, "02")
+				]
+			}else if(oInputModel.getProperty("/claim_item/claim_type_item_id") === this._oConstant.ClaimTypeItem.FLIGHT_WIL){
+				//bind by item
+				// current year - 18, anything < that year = in
+				// current year - 19 and -25 within those range = in filteroperator.bt
+				// 01 = spouse
+				// 02 = child
+			}
+			oBinding.filter(aFilters)
 
 		},
 
@@ -4405,7 +4427,7 @@ sap.ui.define([
 			const aControlIds = [
 				"select_claimdetails_input_depedent_or_anggota",
 				"field_claimdetails_input_anggota_name",
-				"field_claimdetails_input_dependent_name",
+				"select_claimdetails_input_dependent_name",
 				"select_claimdetails_input_type_of_professional_body",
 				"input_claimdetails_input_policy_number",
 				"select_claimdetails_input_funeral_transportation",
@@ -4550,7 +4572,7 @@ sap.ui.define([
 			const aControlIds = [
 				"select_claimdetails_input_depedent_or_anggota",
 				"field_claimdetails_input_anggota_name",
-				"field_claimdetails_input_dependent_name",
+				"select_claimdetails_input_dependent_name",
 				"select_claimdetails_input_type_of_professional_body",
 				"input_claimdetails_input_policy_number",
 				"select_claimdetails_input_funeral_transportation",
