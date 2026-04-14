6f9470f02c3a87c8c7515f95c82ff32c8606216a
Rushdi Abdul Rahman <rushdi.abdul.rahman@my.ey.com>
2026-04-14 13:06:57 +0000

    Revert "remove comment field from claim header required checking"
    This reverts commit 35554861d88619a4539e2b8caf3caeb936939e9a.

--- app/Claim-A/webapp/controller/App.controller.js
+++ app/Claim-A/webapp/controller/App.controller.js
@@ -921,7 +921,8 @@ sap.ui.define([
 			var reqFields = [
 				"input_claiminput_purpose",
 				"datepicker_claiminput_tripstartdate",
-				"datepicker_claiminput_tripenddate"
+				"datepicker_claiminput_tripenddate",
+				"input_claiminput_comment",
 			];
 			//// add attachment field if using email approval
 			if (this.byId("fileuploader_claiminput_attachment").getRequired()) {
