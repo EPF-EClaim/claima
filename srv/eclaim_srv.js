32539a3333e2f45461afed29a689e369ba9ad6b3
Jarod Peh <jarod.peh@my.ey.com>
2026-04-22 11:33:12 +0000

    changed req.error into req.reject

--- srv/eclaim_srv.js
+++ srv/eclaim_srv.js
@@ -885,7 +885,7 @@ module.exports = (srv) => {
             return result;
 
         } catch (error) {
-            return req.error(400, `Fail processing records: ${error.message}`);
+            return req.reject(400, `Fail processing records: ${error.message}`);
         }
     });
     /**
