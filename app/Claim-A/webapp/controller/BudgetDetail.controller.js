87b0731d171b1f0ab6359bb0b5a9ef3f350ac153
Aiman Salim <aiman.salim@my.ey.com>
2026-06-05 08:54:21 +0000

    commit changes

--- app/Claim-A/webapp/controller/BudgetDetail.controller.js
+++ app/Claim-A/webapp/controller/BudgetDetail.controller.js
@@ -1,10 +1,34 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(
-	Controller
-) {
-	"use strict";
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, Filter, FilterOperator) {
+    "use strict";
 
-	return Controller.extend("claima.controller.BudgetDetail", {
-	});
+    return Controller.extend("claima.controller.BudgetDetail", {
+
+        onInit: function () {
+            const oRouter = this.getOwnerComponent().getRouter();
+
+            oRouter.getRoute("ZEMP_CC_BUDGET_DETAIL")
+                .attachPatternMatched(this._onMatched, this);
+        },
+
+        _onMatched: function (oEvent) {
+            const args = oEvent.getParameter("arguments");
+
+            const aFilters = [
+                new Filter("FUND_CENTER", FilterOperator.EQ, args.fundCenter),
+                new Filter("COMMITMENT_ITEM", FilterOperator.EQ, args.glCode),
+                new Filter("MATERIAL_GROUP", FilterOperator.EQ, args.material)
+            ];
+
+            const oTable = this.byId("detailTable");
+
+            if (oTable && oTable.getBinding("items")) {
+                oTable.getBinding("items").filter(aFilters);
+            }
+        }
+
+    });
 });
--- app/Claim-A/webapp/ext/controller/Common.controller.js
+++ app/Claim-A/webapp/ext/controller/Common.controller.js
@@ -11,9 +11,24 @@ sap.ui.define(['sap/ui/core/mvc/ControllerExtension',
 			 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 			 * @memberOf claima.ext.controller.Common
 			 */
-			onInit: function () {
 			
+			onInit: function () {
+				const oExtensionAPI = this.getExtensionAPI();
+
+				oExtensionAPI.attachTableInitialized((oEvent) => {
+
+					const oTable = oEvent.getParameter("table");
+
+					if (oTable && !oTable._rowPressAttached) {
+
+						oTable.attachItemPress(this.onItemPress.bind(this));
+						oTable._rowPressAttached = true;
+
+						console.log("✅ FE table event attached");
+					}
+				});
 			},
+
 
 			editFlow: {
 				onBeforeCreate(mParameters) {
@@ -22,7 +37,7 @@ sap.ui.define(['sap/ui/core/mvc/ControllerExtension',
 					const END_DATE = aData.find(item => item.END_DATE)?.END_DATE;
 					const dStart = new Date(START_DATE);
 					const dEnd = new Date(END_DATE);
-					
+
 					if (START_DATE && END_DATE && dEnd < dStart) {
 						sap.m.MessageToast.show("End date cannot be earlier than start date");
 						return Promise.reject();
@@ -30,6 +45,20 @@ sap.ui.define(['sap/ui/core/mvc/ControllerExtension',
 					return Promise.resolve();
 				}
 			}
+		},
+		onItemPress: function (oEvent) {
+			const oItem = oEvent.getParameter("listItem");
+			const oData = oItem.getBindingContext().getObject();
+
+			const fundCenter = oData.FUND_CENTER;
+			const glCode = oData.COMMITMENT_ITEM;
+			const material = oData.MATERIAL_GROUP;
+
+			this.getExtensionAPI().routing.navigateToRoute("ZEMP_CC_BUDGET_DETAIL", {
+				fundCenter,
+				glCode,
+				material
+			});
 		}
 	});
 });
