11a9831ee44c63c280655a8fcad605e29efbf4d8
Farisha Razak <farisha.razak@my.ey.com>
2026-06-18 07:22:52 +0000

    small changes

--- app/Claim-A/webapp/controller/App.controller.js
+++ app/Claim-A/webapp/controller/App.controller.js
@@ -62,7 +62,6 @@ sap.ui.define([
 		DateUtility: DateUtility,
 
 		onInit: async function () {
-			
 			this._oConstant = this.getOwnerComponent().getModel("constant").getData();
 			this._oRouter = this.getOwnerComponent().getRouter();
 			this._oDataModel = this.getOwnerComponent().getModel();
@@ -162,13 +161,13 @@ sap.ui.define([
 				// End 	 Aiman Salim 10/02/2026 - Added for analytics
 				// Start Aiman Salim 03/03/2026 - Added for MyClaim
 				case "req_summary":
-					//if (bDTDAdmin || bAdminSystem || bAdminCC) {
+					if (bDTDAdmin || bAdminSystem || bAdminCC) {
 						HashChanger.getInstance().replaceHash("");
 						this._oRouter.navTo("ZEMP_REQUEST_REPORT_SUMMARY");
-					//} else {
-					//	var message = Utility.getText("msg_unauthorized_role");
-					//	MessageBox.error(message);
-					//}
+					} else {
+						var message = Utility.getText("msg_unauthorized_role");
+						MessageBox.error(message);
+					}
 					break;
 				case "req_details":
 					if (bDTDAdmin || bAdminSystem || bAdminCC) {
