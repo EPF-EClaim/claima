971c241e7fee5aa2f5fc12fbbb45c0dbe4943b61
Vincent Yong <dick.soon.yong@my.ey.com>
2026-06-25 09:47:05 +0000

    fix auto approve set back to pending approval after submitting

--- app/Claim-A/webapp/utils/ClaimUtility.js
+++ app/Claim-A/webapp/utils/ClaimUtility.js
@@ -704,6 +704,20 @@ sap.ui.define([
 					}
 					
 				});
+		},
+		
+		fetchAutoClaimStatus: async function(sClaimID){
+			const oContext = this._oView.getModel().bindContext("/checkClaimHeaderStatusForAutoApproval(...)");
+			oContext.setParameter("sClaimID", sClaimID);
+
+			try{
+				await oContext.execute();
+				const oResult = oContext.getBoundContext().getObject();
+				
+				return oResult.sStatus;
+			}catch(oError){
+				return null;
+			}
 		}
 		
 	}
