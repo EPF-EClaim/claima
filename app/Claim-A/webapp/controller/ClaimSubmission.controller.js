b6ab523fc3e52cce243a8a65f60782a5b2283287
Kevin Choi <kevin.choi@my.ey.com>
2026-09-21 08:14:56 +0000

    CL052: Adding error message for amount zero to CustomValidator

--- app/Claim-A/webapp/controller/ClaimSubmission.controller.js
+++ app/Claim-A/webapp/controller/ClaimSubmission.controller.js
@@ -2869,20 +2869,12 @@ sap.ui.define([
 			var oInputModel = this.getView().getModel("claimitem_input");
 			var oClaimSubmissionModel = this.getView().getModel("claimsubmission_input");
 
-			// the Amount (MYR) must not be zero (e.g. rate could not be determined for that date).
-			if (parseFloat(oInputModel.getProperty("/claim_item/amount")) <= 0) {
-				MessageBox.error(Utility.getText("msg_claimdetails_amount_zero"), {
-					closeOnBrowserNavigation: false
-				});
-				return;
-			}
-
-
 			CustomValidator.init(this.getOwnerComponent(), this.getView());
-			var bCanProceed = await CustomValidator.validate(this._oConstant.SubmissionTypePrefix.CLAIMHEADER);
+			var bCanProceed = await CustomValidator.validate(this._oConstant.SubmissionTypePrefix.CLAIM);
 			if (!bCanProceed) {
 				return;
 			}
+
 			//if departure time and arrival time exist, it will do a calculation for the flight duration
 			//this is needed for the eligibility check for the flight class of the employee
 			//setting the flight hours into the no_of_hours field in the model to allow the eligibility payload generation code to retrieve the flight hours value
